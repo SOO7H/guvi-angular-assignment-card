d14cd53abdbc370e98da474a1ff73208d00cde01
MILIND JHA <milind.jha.20@gmail.com>
2023-10-13 12:33:21 +0530

    commits

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,6 +10,7 @@ export class CardComponent {
   @Input() plan: string = "";
   @Input() price: string = "";
   @Input() users: string = "";
+  @Input() isBold: boolean = true;
   @Input() storage: string = "";
   @Input() unlimited_private_projects: Array<string> = [];
   @Input() dedicated_phone_support: Array<string> = [];
